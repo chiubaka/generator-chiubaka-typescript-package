58fd7866a6f5d36d6c79886539f3a633913a3725
Daniel Chiu <daniel@chiubaka.com>
2022-07-03 00:47:21 -0400

    Create barebones yeoman generator using TypeScript

---
+++ src/app/index.ts
@@ -0,0 +1,7 @@
+import Generator, { GeneratorOptions } from "yeoman-generator";
+
+export default class extends Generator {
+  constructor(args: string | string[], opts: GeneratorOptions) {
+    super(args, opts);
+  }
+};
