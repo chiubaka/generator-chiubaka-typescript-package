f50a87f536789466b8b84723772b1e2bf972b87e
Daniel <1732408+chiubaka@users.noreply.github.com>
2022-07-05 15:32:25 -0400

    Generate a full package.json file (#18)
    * Add a few more options to the NodeModuleGenerator
    
    * Fill in scripts, repository, bugs, and homepage sections of package.json
    
    * Add generation of keywords section of package.json

--- src/node-module/NodeModuleGenerator.ts
+++ src/node-module/NodeModuleGenerator.ts
@@ -1,8 +1,13 @@
 import assert from "node:assert";
-import Generator, { GeneratorOptions } from "yeoman-generator";
+import Generator, { GeneratorOptions, Questions } from "yeoman-generator";
 
 interface PromptAnswers {
-  packageName: string;
+  name: string;
+  description: string;
+  authorName: string;
+  authorEmail: string;
+  githubUrl: string;
+  keywords?: string;
 }
 
 export class NodeModuleGenerator extends Generator {
@@ -13,19 +18,56 @@ export class NodeModuleGenerator extends Generator {
   }
 
   public async prompting() {
-    this.answers = await this.prompt({
-      type: "input",
-      name: "packageName",
-      message: "What's the name of this new package?",
-    });
+    const questions: Questions<PromptAnswers> = [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of this new package?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "What is the description of this new package?",
+      },
+      {
+        type: "input",
+        name: "authorName",
+        message: "Who is the author of this new package?",
+      },
+      {
+        type: "input",
+        name: "authorEmail",
+        message: "What is the email address of the author of this new package?",
+      },
+      {
+        type: "input",
+        name: "githubUrl",
+        message: "What is the full URL of the GitHub repo for the new package?",
+      },
+      {
+        type: "input",
+        name: "keywords",
+        message:
+          "What keywords would you like to associate with this new package?",
+      },
+    ];
+
+    this.answers = await this.prompt(questions);
   }
 
   public writing() {
     assert(this.answers);
+
+    const keywordTokens = this.answers.keywords?.split(" ") || [];
+    const keywords = keywordTokens.map((token) => `"${token}"`).join(", ");
+
     this.fs.copyTpl(
       this.templatePath("package.json.ejs"),
       this.destinationPath("package.json"),
-      { name: this.answers.packageName }
+      {
+        ...this.answers,
+        keywords,
+      }
     );
   }
 }
