6cd64fb2b91a5a1f0ed73cacf104ec681bfed9aa
Daniel Chiu <daniel@chiubaka.com>
2022-07-06 15:09:48 -0400

    Add TsConfigGenerator to global main generator

--- src/app/TypeScriptPackageGenerator.test.ts
+++ src/app/TypeScriptPackageGenerator.test.ts
@@ -11,11 +11,23 @@ describe("TypeScriptPackageGenerator", () => {
       .run();
   });
 
-  it("creates a package.json file", () => {
-    result.assertFile("package.json");
+  describe("package.json", () => {
+    it("creates a package.json file", () => {
+      result.assertFile("package.json");
+    });
+
+    describe("installs required devDependencies", () => {
+      it("installs @chiubaka/tsconfig", () => {
+        expect(result).toHaveDevDependency("@chiubaka/tsconfig", "0.0.2");
+      });
+    });
   });
 
   it("creates a .gitignore file", () => {
     result.assertFile(".gitignore");
+  });
+
+  it("creates a tsconfig.json file", () => {
+    result.assertFile("tsconfig.json");
   });
 });
