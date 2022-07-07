239762a0665e03534589beb05e78ce483fdff7db
Daniel Chiu <daniel@chiubaka.com>
2022-07-07 16:05:03 -0400

    Update Jest timeout for test environment

--- src/__tests__/__setup__/jest.setup.ts
+++ src/__tests__/__setup__/jest.setup.ts
@@ -1,5 +1,9 @@
 import { matchers } from "./matchers";
 
-expect.extend(matchers);
+// We are actually running full yarn installs in the generated project in tests, and
+// that does take some time on CI
+const JEST_TIMEOUT = process.env.NODE_ENV !== "test" ? 10_000 : 60_000;
 
-jest.setTimeout(10_000);
+jest.setTimeout(JEST_TIMEOUT);
+
+expect.extend(matchers);
