6189c8373f219fc1fe67c83bf0b4e33e12d6bc99
Daniel Chiu <daniel@chiubaka.com>
2022-07-15 18:09:12 -0400

    Move typescript generator tests to unit tests

--- test/unit/typescript/TypeScriptGenerator.test.ts
+++ test/unit/typescript/TypeScriptGenerator.test.ts
@@ -1,10 +1,13 @@
+import path from "node:path";
 import YeomanHelpers, { RunResult } from "yeoman-test";
 
 describe("TypeScriptGenerator", () => {
   let result: RunResult;
 
   beforeAll(async () => {
-    result = await YeomanHelpers.create(__dirname).run();
+    result = await YeomanHelpers.create(
+      path.join(__dirname, "../../../src/typescript")
+    ).run();
   });
 
   describe("installs required devDependencies", () => {
