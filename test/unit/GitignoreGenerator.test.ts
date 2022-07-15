06cf4d0b7a02e618b557818ce039b571962698e0
Daniel Chiu <daniel@chiubaka.com>
2022-07-15 17:20:07 -0400

    Move GitignoreGenerator.test.ts to unit tests

--- test/unit/GitignoreGenerator.test.ts
+++ test/unit/GitignoreGenerator.test.ts
@@ -1,10 +1,13 @@
+import path from "node:path";
 import YeomanHelpers, { RunResult } from "yeoman-test";
 
 describe("GitignoreGenerator", () => {
   let result: RunResult;
 
   beforeAll(async () => {
-    result = await YeomanHelpers.create(__dirname).run();
+    result = await YeomanHelpers.create(
+      path.join(__dirname, "../../src/gitignore")
+    ).run();
   });
 
   it("creates a .gitignore file", () => {
