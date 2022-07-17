8a56f94b481cabc6121c138eab45b6edfc69680b
Daniel <1732408+chiubaka@users.noreply.github.com>
2022-07-17 00:51:19 -0400

    Attempt to fix issues in submodule deploy (#137)
    * Initialize submodules for publish job
    
    * Ensure a user name and email is set for git
    
    * Update package name for generated typescript package to chiubaka scope
    
    * Update generated package version to match generator version

--- scripts/deploySubmodule.ts
+++ scripts/deploySubmodule.ts
@@ -1,33 +1,77 @@
 import { program } from "commander";
 import { spawnSync } from "node:child_process";
+import fs from "node:fs";
 import path from "node:path";
 
-import PackageJson from "../package.json";
+import packageJson from "../package.json";
 import { GitGenerator } from "../src/git";
 
-function main() {
-  program.argument("submodule", "name of the submodule to deploy");
-  program.parse();
+class SubmoduleDeployer {
+  private submodulePath: string;
 
-  const { args } = program;
-  const [submodule] = args;
+  constructor() {
+    program.argument("submodule", "name of the submodule to deploy");
+    program.parse();
+    const { args } = program;
+    const [submodule] = args;
 
-  const commitMessage = GitGenerator.generateCommitMessage();
+    this.submodulePath = path.join(__dirname, `../generated`, submodule);
+  }
 
-  runInSubmodule(submodule, "git", ["add", "."]);
-  runInSubmodule(submodule, "git", ["commit", "-m", commitMessage]);
-  runInSubmodule(submodule, "git", ["tag", `v${PackageJson.version}`]);
-  runInSubmodule(submodule, "git", ["push"]);
-  runInSubmodule(submodule, "git", ["push", "--tags"]);
+  public run() {
+    this.updateSubmoduleVersion();
+    this.configureGitUser();
+    this.gitCommit();
+    this.gitTag();
+    this.gitPush();
+  }
+
+  private updateSubmoduleVersion() {
+    const submodulePackageJsonPath = path.join(
+      this.submodulePath,
+      "package.json"
+    );
+    // eslint-disable-next-line @typescript-eslint/no-var-requires, security/detect-non-literal-require
+    const submodulePackageJson = require(submodulePackageJsonPath) as {
+      version: string;
+    };
+
+    submodulePackageJson.version = packageJson.version;
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    fs.writeFileSync(
+      submodulePackageJsonPath,
+      JSON.stringify(submodulePackageJson, undefined, 2)
+    );
+  }
+
+  private configureGitUser() {
+    this.runInSubmodule("git", ["config", "user.name", "Daniel Chiu"]);
+    this.runInSubmodule("git", ["config", "user.email", "daniel@chiubaka.com"]);
+  }
+
+  private gitCommit() {
+    const commitMessage = GitGenerator.generateCommitMessage();
+
+    this.runInSubmodule("git", ["add", "."]);
+    this.runInSubmodule("git", ["commit", "-m", commitMessage]);
+  }
+
+  private gitTag() {
+    this.runInSubmodule("git", ["tag", `v${packageJson.version}`]);
+  }
+
+  private gitPush() {
+    this.runInSubmodule("git", ["push"]);
+    this.runInSubmodule("git", ["push", "--tags"]);
+  }
+
+  private runInSubmodule(command: string, args: string[]) {
+    spawnSync(command, args, {
+      cwd: this.submodulePath,
+      stdio: "inherit",
+    });
+  }
 }
 
-function runInSubmodule(submodule: string, command: string, args: string[]) {
-  const submodulePath = path.join(__dirname, `../generated`, submodule);
-
-  spawnSync(command, args, {
-    cwd: submodulePath,
-    stdio: "inherit",
-  });
-}
-
-main();
+const deployer = new SubmoduleDeployer();
+deployer.run();
