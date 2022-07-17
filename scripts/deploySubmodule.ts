7dbff512fc13297a4c546493c40a7257ab1ddff1
Daniel <1732408+chiubaka@users.noreply.github.com>
2022-07-16 21:35:31 -0400

    Write some scripts to automatically generate, commit, and push updates to generated packages (#136)

---
+++ scripts/deploySubmodule.ts
@@ -0,0 +1,33 @@
+import { program } from "commander";
+import { spawnSync } from "node:child_process";
+import path from "node:path";
+
+import PackageJson from "../package.json";
+import { GitGenerator } from "../src/git";
+
+function main() {
+  program.argument("submodule", "name of the submodule to deploy");
+  program.parse();
+
+  const { args } = program;
+  const [submodule] = args;
+
+  const commitMessage = GitGenerator.generateCommitMessage();
+
+  runInSubmodule(submodule, "git", ["add", "."]);
+  runInSubmodule(submodule, "git", ["commit", "-m", commitMessage]);
+  runInSubmodule(submodule, "git", ["tag", `v${PackageJson.version}`]);
+  runInSubmodule(submodule, "git", ["push"]);
+  runInSubmodule(submodule, "git", ["push", "--tags"]);
+}
+
+function runInSubmodule(submodule: string, command: string, args: string[]) {
+  const submodulePath = path.join(__dirname, `../generated`, submodule);
+
+  spawnSync(command, args, {
+    cwd: submodulePath,
+    stdio: "inherit",
+  });
+}
+
+main();
